6ec0377609941d8bf31d06cf45815fe9d86ae170
tovsky <pavel.hr@mail.ru>
2020-08-14 10:29:50 +0300

    EM-1 [feature] Добавлены шаблоны стили шрифты контроллеры

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserRegistrationFormType;
+use App\Security\LoginFormAuthenticator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register()
+//    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $formAuthenticator)
+    {
+//        $form =$this->createForm(UserRegistrationFormType::class);
+//
+//
+//        $form->handleRequest($request);
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            /** @var User $user $user */
+//            $user = $form->getData();
+//
+//            $user->setPassword($passwordEncoder->encodePassword(
+//                $user,
+//                $form['plainPassword']->getData()
+//            ));
+//
+//            if (true === $form['agreeTerms']->getData()) {
+////                $user->setAgreedTermsAt(new \DateTime());
+//                $user->agreeTerms();
+//            }
+//
+//            $em = $this->getDoctrine()->getManager();
+//            $em->persist($user);
+//            $em->flush();
+//
+////            return $this->redirectToRoute('app_account');
+//            return $guardHandler->authenticateUserAndHandleSuccess(
+//                $user,
+//                $request,
+//                $formAuthenticator,
+//                'main'
+//            );
+//        }
+//
+//        return $this->render('security/register.html.twig', [
+//            'registrationForm' => $form->createView()
+//        ]);
+    }
+
+
+}
