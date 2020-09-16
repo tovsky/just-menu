2cd9fee651921469da960b0969afdbbcd2f6a019
tovsky <pavel.hr@mail.ru>
2020-09-16 09:23:47 +0300

    EM-20 [feature] Добавлен эндпойнт получения всех ресторанов пользователя

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -93,7 +93,7 @@ class User implements UserInterface
     private $isVerified = false;
 
     /**
-     * @ORM\ManyToMany(targetEntity=Restaurant::class, mappedBy="users")
+     * @ORM\ManyToMany(targetEntity=Restaurant::class, mappedBy="users", cascade={"persist"})
      */
     private $restaurants;
 
